25c81d9d5f4784589c64c6ae30d30a3113dce748
baobaote00 <baobaote00@gmail.com>
2022-10-26 15:15:38 +0900

    :heart:

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = require("./routes");
 
 const app = express();
-const port = process.env.PORT || "1337";
+const port = "1337";
 app.set("port", port);
 
 app.use('/', routes);
