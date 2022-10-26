19f1d953e1cb1a97e7e24796e688ffd7a0b0d6d8
baobaote00 <baobaote00@gmail.com>
2022-10-26 16:38:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = require("./routes");
 
 const app = express();
-const port = "1337";
+const port = process.env.PORT || "1337";
 app.set("port", port);
 
 app.use('/', routes);
