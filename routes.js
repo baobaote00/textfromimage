3ce84e019509656b0072c772c4b92a80f97d71b3
baobaote00 <baobaote00@gmail.com>
2022-10-26 15:30:27 +0900

    :heart:

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get("/todos", function(req, res) {
+router.get("/", function(req, res) {
   res.send('Hello World!')
 });
 
