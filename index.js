fc7669a0a305ea94dc8bcb0c1e1fd03f8212a4ec
baobaote00 <baobaote00@gmail.com>
2022-10-27 23:02:48 +0900

    :heart: update cors

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 app.use(fileupload());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 const port = process.env.PORT || "1337";
 app.set("port", port);
 
