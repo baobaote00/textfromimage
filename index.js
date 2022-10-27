7b2a1d65a5595e2fd5253ecaaf0a77972f960504
baobaote00 <baobaote00@gmail.com>
2022-10-27 22:54:25 +0900

    :heart: final demo

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const routes = require("./routes");
+var fileupload = require("express-fileupload");
+var bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 const app = express();
+//if you want in every domain then
+app.use(cors())
+app.use(bodyParser.json());
+app.use(fileupload());
 const port = process.env.PORT || "1337";
 app.set("port", port);
 
--- routes.js
+++ routes.js
@@ -1,9 +1,33 @@
 const express = require('express');
-
+const { createWorker } = require("tesseract.js")
 const router = express.Router();
+const fs = require('fs')
+const { translate } = require('free-translate');
+const worker = createWorker();
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.send('Hello test!')
+});
+
+(async () => {
+  await worker.load();
+  await worker.loadLanguage('jpn');
+  await worker.initialize('jpn');
+})();
+
+router.post('/uploadfile', (req, res) => {
+  (async () => {
+    await fs.writeFileSync('image.png', req.files.image.data, (err) => {
+      // throws an error, you could also catch it here
+      if (err) throw err;
+    });
+
+    const { data: { text } } = await worker.recognize('./image.png');
+    console.log(text);
+    const translatedText = await translate(text, { from: 'ja', to: 'en' });
+    console.log(translatedText);
+    res.json({ text: text, translatedText: translatedText })
+  })();
 });
 
 module.exports = router;
